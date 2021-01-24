4bbbf3e7669d49559a1048d4a19c43e6bac6be7d
Edwin Torres <edwin_torres@icloud.com>
2021-01-23 21:16:46 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -96,26 +96,26 @@ class App extends Component {
 						<Button handleClick={this.multiply}> * </Button>
 					</div>
 					<div className="row">
-						<Button handleClick={this.addToInput}> 7 </Button>
-						<Button handleClick={this.addToInput}> 8 </Button>
-						<Button handleClick={this.addToInput}> 9 </Button>
+						<Button handleClick={this.addToInput}>7</Button>
+						<Button handleClick={this.addToInput}>8</Button>
+						<Button handleClick={this.addToInput}>9</Button>
 						<Button handleClick={this.subtract}> - </Button>
 					</div>
 					<div className="row">
-						<Button handleClick={this.addToInput}> 4 </Button>
-						<Button handleClick={this.addToInput}> 5 </Button>
-						<Button handleClick={this.addToInput}> 6 </Button>
+						<Button handleClick={this.addToInput}> 4</Button>
+						<Button handleClick={this.addToInput}>5</Button>
+						<Button handleClick={this.addToInput}>6</Button>
 						<Button handleClick={this.add}> + </Button>
 					</div>
 					<div className="row">
-						<Button handleClick={this.addToInput}> 1 </Button>
-						<Button handleClick={this.addToInput}> 2 </Button>
-						<Button handleClick={this.addToInput}> 3 </Button>
+						<Button handleClick={this.addToInput}>1</Button>
+						<Button handleClick={this.addToInput}>2</Button>
+						<Button handleClick={this.addToInput}>3</Button>
 						<Button handleClick={this.evaluate}> = </Button>
 					</div>
 					<div className="row">
 						<Button Button handleClick={this.addZeroToInput}> 0 </Button>
-						<Button handleClick={this.addDecimal}> . </Button>
+						<Button handleClick={this.addDecimal}>.</Button>
 					</div>
 				</div>
 			</div>
